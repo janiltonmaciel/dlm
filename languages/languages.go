d4e3071012fbc67d329fcc6a4eb9578a443b9332
Janilton Maciel <janilton@corp.globo.com>
2018-06-12 17:56:16 -0300

    core: refactory languages options

--- languages/languages.go
+++ languages/languages.go
@@ -1,5 +1,7 @@
 package languages
 
+import "strings"
+
 type Language interface {
 	Name() string
 	Help() string
@@ -7,9 +9,31 @@ type Language interface {
 	GetDockerfile(version string) string
 }
 
-var Choices = []Language{
+var languages = []Language{
 	Node{},
 	Python{},
 	Ruby{},
 	Golang{},
+}
+
+var languageChoices []string
+
+func init() {
+	for _, lang := range languages {
+		languageChoices = append(languageChoices, lang.Name())
+	}
+
+}
+
+func GetLanguages() []string {
+	return languageChoices
+}
+
+func GetLanguage(name string) Language {
+	for _, lang := range languages {
+		if strings.ToLower(lang.Name()) == strings.ToLower(name) {
+			return lang
+		}
+	}
+	return nil
 }
--- main.go
+++ main.go
@@ -1,101 +1,36 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"io/ioutil"
 	"os"
-	"strings"
 
 	"github.com/janiltonmaciel/dockerfile-gen/core"
 	"github.com/janiltonmaciel/dockerfile-gen/languages"
+
 	"gopkg.in/AlecAivazis/survey.v1"
 	surveyCore "gopkg.in/AlecAivazis/survey.v1/core"
 )
 
 func init() {
-	surveyCore.ErrorTemplate = `{{color "red"}}{{ ErrorIcon }} {{.Error}}{{color "reset"}}
-	`
-	survey.MultiSelectQuestionTemplate = `
-	{{- if .ShowHelp }}{{- color "cyan"}}{{ HelpIcon }} {{ .Help }}{{color "reset"}}{{"\n"}}{{end}}
-	{{- color "green+hb"}}{{ QuestionIcon }} {{color "reset"}}
-	{{- color "default+hb"}}{{ .Message }}{{ .FilterMessage }}{{color "reset"}}
-	{{- if .ShowAnswer}}{{color "cyan"}} {{.Answer}}{{color "reset"}}{{"\n"}}
-	{{- else }}
-		{{- "  "}}{{- color "cyan"}}{{- if and .Help (not .ShowHelp)}}[ {{ HelpInputRune }} for more help]{{end}}{{color "reset"}}
-	  {{- "\n"}}
-	  {{- range $ix, $option := .PageEntries}}
-		{{- if eq $ix $.SelectedIndex}}{{color "cyan"}}{{ SelectFocusIcon }}{{color "reset"}}{{else}} {{end}}
-		{{- if index $.Checked $option}}{{color "green"}} {{ MarkedOptionIcon }} {{else}}{{color "default+hb"}} {{ UnmarkedOptionIcon }} {{end}}
-		{{- color "reset"}}
-		{{- " "}}{{$option}}{{"\n"}}
-	  {{- end}}
-	{{- end}}`
-
-	survey.InputQuestionTemplate = `
-	{{- if .ShowHelp }}{{- color "cyan"}}{{ HelpIcon }} {{ .Help }}{{color "reset"}}{{"\n"}}{{end}}
-	{{- color "green+hb"}}{{ QuestionIcon }} {{color "reset"}}
-	{{- color "default+hb"}}{{ .Message }} {{color "reset"}}
-	{{- if .ShowAnswer}}
-	  {{- color "cyan"}}{{.Answer}}{{color "reset"}}{{"\n"}}
-	{{- else }}
-	  {{- if .Default}}{{color "white"}}({{.Default}}) {{color "reset"}}{{end}}
-	  {{- if and .Help (not .ShowHelp)}}{{color "cyan"}}[{{ HelpInputRune }} for help]{{color "reset"}} {{end}}
-	{{- end}}`
-
-}
-
-type Cli struct {
-	languages []languages.Language
-	options   []string
-}
-
-func NewCli() Cli {
-	cli := Cli{}
-	cli.languages = languages.Choices
-
-	for _, lang := range cli.languages {
-		cli.options = append(cli.options, lang.Name())
-	}
-
-	return cli
-}
-
-func (c *Cli) GetOptions() []string {
-	return c.options
-}
-
-func (c Cli) GetLanguage(name string) languages.Language {
-	for _, lang := range c.languages {
-		if strings.ToLower(lang.Name()) == strings.ToLower(name) {
-			return lang
-		}
-	}
-	return nil
+	surveyCore.ErrorTemplate = core.ErrorTemplate
+	survey.MultiSelectQuestionTemplate = core.MultiSelectQuestionTemplate
+	survey.InputQuestionTemplate = core.InputQuestionTemplate
 }
 
 func main() {
-	cli := NewCli()
-
 	answers := []string{}
 	prompt := &survey.MultiSelect{
-		Message: "Quais as linguagens de sua imagem:",
-		Options: cli.GetOptions(),
-		// Help:    "Phone number should include the area code",
+		Message: "Select the programming languages:",
+		Options: languages.GetLanguages(),
 	}
-	validate := func(val interface{}) error {
-		if arr, ok := val.([]string); !ok || len(arr) <= 0 {
-			return errors.New("Selecione as linguagen(s)")
-		}
-		return nil
-	}
-	survey.AskOne(prompt, &answers, validate)
+	survey.AskOne(prompt, &answers, survey.Required)
 
 	var context = make(map[string]string)
 	var lang languages.Language
 	var version string
 	for _, answer := range answers {
-		lang = cli.GetLanguage(answer)
+		lang = languages.GetLanguage(answer)
 		if lang == nil {
 			os.Exit(1)
 		}
@@ -106,7 +41,7 @@ func main() {
 			Help:    lang.Help(),
 			Default: lang.Default(),
 		}
-		survey.AskOne(prompt, &version, nil)
+		survey.AskOne(prompt, &version, survey.Required)
 
 		context[lang.Name()] = lang.GetDockerfile(version)
 	}
@@ -118,7 +53,7 @@ func main() {
 		Help:    "Alpine libs",
 		Default: ExtraLibs,
 	}
-	survey.AskOne(p, &libs, nil)
+	err := survey.AskOne(p, &libs, nil)
 	context["Libs"] = libs
 
 	dockerfile := core.ParseTemplate(context, "Build")
