63d7e5382214b85f53e1159f0c09ac5e34d4b8a9
Janilton Maciel <janilton@gmail.com>
2019-05-21 08:08:47 -0300

    add alias ls

--- cmd/list.go
+++ cmd/list.go
@@ -9,10 +9,11 @@ import (
 	"gopkg.in/gookit/color.v1"
 )
 
-func NewCommandList() cli.Command {
+func newCommandList() cli.Command {
 	return cli.Command{
-		Name:  "list",
-		Usage: "List versions available for docker language",
+		Name:    "list",
+		Aliases: []string{"ls"},
+		Usage:   "List versions available for docker language",
 		UsageText: `
     dfm list <language>                 # List versions available for docker
         --pre-release                   When listing, show pre-release version
@@ -22,8 +23,8 @@ func NewCommandList() cli.Command {
 Examples:
    dfm list golang --pre-release        # List versions available for docker golang with pre-release
    dfm list python 3.7                  # List versions available for docker python, matching version 3.7
-   dfm list python 3 --pre-release      # List versions available for docker python, matching version 3
-   dfm list node 8                      # List versions available for docker node, matching version 8.15
+   dfm list python 3 --pre-release      # List versions available for docker python with pre-release, matching version 3
+   dfm list node 8                      # List versions available for docker node, matching version 8
 `,
 		Flags: []cli.Flag{
 			cli.BoolFlag{
@@ -36,34 +37,25 @@ Examples:
 }
 
 func listAction(c *cli.Context) error {
+
 	if c.NArg() <= 0 {
 		return showCommandHelp(c)
 	}
 
 	languageInput := strings.TrimSpace(c.Args().Get(0))
-	if languageInput == "" {
+	if languageInput == "" || strings.ToLower(languageInput) == "help" {
 		return showCommandHelp(c)
 	}
 
 	language := core.GetLanguage(languageInput)
 	if language == nil {
-		msg := fmt.Sprintf("%s %s",
-			color.FgRed.Render("Language invalid:"),
-			color.FgYellow.Render(languageInput))
-		fmt.Fprintln(c.App.Writer, msg)
-		fmt.Fprintln(c.App.Writer)
-		return cli.ShowCommandHelp(c, "languages")
+		return showLanguageCommandHelp(c, languageInput)
 	}
 
 	versionInput := strings.TrimSpace(c.Args().Get(1))
 	withPrerelease := c.Bool("pre-release")
-	versions := core.FindVersions(language.Name, withPrerelease, versionInput)
+	printVersions(language, withPrerelease, versionInput)
 
-	for _, version := range versions {
-		fmt.Printf("%25s   %s\n",
-			color.FgGreen.Render(version.Version),
-			color.FgDefault.Render(version.DistributionReleases))
-	}
 	return nil
 }
 
@@ -71,4 +63,22 @@ func showCommandHelp(c *cli.Context) error {
 	fmt.Fprintln(c.App.Writer, color.FgRed.Render("Incorrect usage!"))
 	fmt.Fprintln(c.App.Writer)
 	return cli.ShowCommandHelp(c, c.Command.Name)
+}
+
+func showLanguageCommandHelp(c *cli.Context, languageInput string) error {
+	msg := fmt.Sprintf("%s %s",
+		color.FgRed.Render("Language invalid:"),
+		color.FgYellow.Render(languageInput))
+	fmt.Fprintln(c.App.Writer, msg)
+	fmt.Fprintln(c.App.Writer)
+	return cli.ShowCommandHelp(c, "languages")
+}
+
+func printVersions(language *core.Language, withPrerelease bool, versionInput string) {
+	versions := core.FindVersions(language.Name, withPrerelease, versionInput)
+	for _, version := range versions {
+		fmt.Printf("%25s   %s\n",
+			color.FgGreen.Render(version.Version),
+			color.FgDefault.Render(version.DistributionReleases))
+	}
 }
